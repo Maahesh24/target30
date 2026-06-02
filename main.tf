99df4001caeb80b8ef152425f570901e2945e339
Maahesh24 <92706965+Maahesh24@users.noreply.github.com>
2026-06-02 17:41:48 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,19 +1,11 @@
-terraform {
-  required_providers {
-    azurerm = {
-      source  = "hashicorp/azurerm"
-      version = "~>4.1.0"
-    }
-  }
-}
-
 provider "azurerm" {
   subscription_id = "711e533a-2f13-498c-8d61-1c1dd88166ae"
+  tenant_id       = var.tenant_id
   features {}
 }
 
-resource "azurerm_resource_group" "nik" {
-  name     = "mahesh-demo-rg"
-  location = "East US"
+variable "tenant_id" {
+  description = "Azure Tenant ID"
+  type        = string
+  sensitive   = true
 }
-
