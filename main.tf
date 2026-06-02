4c3b30b868a1059e58b7dee9413e3d673a026741
Mahesh Potdar <maahesh@gmail.com>
2026-06-02 11:27:55 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,10 +2,24 @@ terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "=4.1.0"
+      version = "~>4.1.0"
     }
   }
-}   
+}
+
 provider "azurerm" {
   features {}
+}
+
+resource "azurerm_resource_group" "rg255" {
+  name     = "mahesh-demo-rg"
+  location = "East US"
+}
+
+resource "azurerm_storage_account" "sa255" {
+  name                     = "maheshstorageacct255"
+  resource_group_name      = azurerm_resource_group.rg255.name
+  location                 = azurerm_resource_group.rg255.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
 }
