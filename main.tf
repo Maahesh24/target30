22969bba494cd002e61b6a6e8fb1461b6a7b7a98
Maahesh24 <92706965+Maahesh24@users.noreply.github.com>
2026-06-02 19:03:28 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,8 +1,9 @@
+
+
 provider "azurerm" {
-  subscription_id = "711e533a-2f13-498c-8d61-1c1dd88166ae"
-  tenant_id       = var.tenant_id
   features {}
 }
+
 
 variable "tenant_id" {
   description = "Azure Tenant ID"
